7e2fa7ecb353d6399c46cddc66ace75c3ac3f5fa
Sai Kumar <saikumar.gopisetty@ebutor.com>
2018-09-26 20:45:48 +0530

    logic changes

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 "use strict"
-module.exports = function phoneNumber(phone_no){
+
+const phoneNumber = (phone_no) => {
     return new Promise((resolve, reject) => {
-        if(phone_no === null || phone_no <= 10 ){
-            reject("Phone must container at least 10 digist."); 
-            console.log("Reject phone number with space");
+        phone_no = phone_no.toString();
+        if(phone_no === null || phone_no.length <= 10 ){
+            return reject("Phone must container at least 10 digist."); 
         }
-        phone_no.toString();
-        phone_no_len = phone_no.length;
+        let phone_no_len = phone_no.length;
         let phoneWithSpace = "";
-        for(i; i < phone_no_len; i++) {
-            phoneWithSpace+= str[i]+" ,";
+        for(var i = 0; i < phone_no_len; i++) {
+            phoneWithSpace+= phone_no[i]+" ,";
         }
-        console.log("phone number with space",phoneWithSpace);
-        resolve(phoneWithSpace);
+        return resolve(phoneWithSpace);
     });
 }
+
+module.exports.phoneNumber = phoneNumber; 
